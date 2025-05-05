fa18954594ed864d9f0ce78b5c8954fe6b4be1ca
jonas.moeneclaey <jonas.moeneclaey@student.staam.be>
2025-05-05 11:51:26 +0200

    1st push

--- script/script.js
+++ script/script.js
@@ -5,18 +5,42 @@ const groenten = [
     prijs: 2.1,
   },
   {
-    id: 1,
+    id: 2,
     naam: "brocoli",
     prijs: 1.5,
   },
   {
-    id: 1,
+    id: 3,
     naam: "tomaat",
     prijs: 3.4,
   },
   {
-    id: 1,
+    id: 4,
     naam: "erwten",
     prijs: 0.7,
   },
 ];
+
+const lijstGroenten = document.querySelector("#lijst")
+const lijstKostprijs = document.querySelector("#kostprijs")
+const groentenSel = document.createElement("select")
+
+lijstGroenten.appendChild(groentenSel)
+
+groenten.forEach((groente) => {
+  const groenteOptie = document.createElement("option")
+  groenteOptie.innerHTML = groente.naam
+  groentenSel.appendChild(groenteOptie)
+  })
+
+const kostprijs = groenten.forEach((prijs) => {
+  return prijs.prijs
+}) 
+
+
+groentenSel.addEventListener(("click"),  => {
+  const groentePrijsP = document.createElement("P")
+  groentePrijsP.innerHTML = kostprijs
+  lijstKostprijs.appendChild(groentePrijsP)
+})
+
